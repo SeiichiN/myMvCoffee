ebecbcdc88f7bcb3afb30e1ea9bab2e25859f4af
vagrant <vagrant@vagrant.vm>
2018-11-06 03:54:00 +0000

    top page dekita.

--- index.php
+++ index.php
@@ -257,7 +257,7 @@
 
     <div class="footer__social">
       <ul class="text-center clearfix">
-        <li><a href="https://www.facebook.com/wp.business.book" target="_blank"><img src="./img/btn-facebook.png" alt="Facebook"/></a></li>
+        <li><a href="https://www.facebook.com/wp.business.book" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/btn-facebook.png" alt="Facebook"/></a></li>
         <li><a href="#" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/btn-twitter.png" alt="Twitter"/></a></li>
         <li><a href="#" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/btn-instagram.png" alt="instagram"/></a></li>
       </ul>
